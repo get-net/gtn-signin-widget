0f23961a555ee6ab38561c9f997ace399fa822c0
arsenitem <atemnik@get-net.ru>
2021-04-16 17:00:57 +0500

    arrow icon style in select

--- src/widget/App.svelte
+++ src/widget/App.svelte
@@ -59,6 +59,19 @@
         border-radius: 0.25rem;
         transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
     }
+	.gtn-widget-wrapper :global(.widget-select) {
+		-webkit-appearance: none;
+		-moz-appearance: none;
+		background: transparent;
+		background-image: url("data:image/svg+xml;utf8,<svg fill='black' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>");
+		background-repeat: no-repeat;
+		background-position-x: 100%;
+		background-position-y: 10px;
+		border: 1px solid #dfdfdf;
+		border-radius: 2px;
+		margin-right: 2rem;
+		padding-right: 2rem;
+	}
     .gtn-widget-wrapper :global(.widget-input:focus) {
         background-color: #fff;
         border-color: #8ceabb;
